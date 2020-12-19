6c9b42c5847c718c045e9548a59203d2e5bc9e76
sid-chaubs <sidchaubal7@gmail.com>
2020-12-19 21:58:24 +0100

    Add trainer

--- architecture/environment.py
+++ architecture/environment.py
@@ -8,20 +8,22 @@ import torchvision.transforms as T
 class Environment:
   
   def __init__(self):
-    self.gym = gym.make('CartPole-v0').unwrapped
+    self.env = gym.make('CartPole-v0').unwrapped
+    self.env.reset()
+
     self.resize = T.Compose([T.ToPILImage(), T.Resize(40, interpolation = Image.CUBIC), T.ToTensor()])
     self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
   def get_screen(self):
-    screen = self.gym.render(mode = 'rgb_array').transpose((2, 0, 1))
+    screen = self.env.render(mode = 'rgb_array').transpose((2, 0, 1))
 
     # cart is in the lower half, so strip off the top and bottom of the screen
     _, screen_height, screen_width = screen.shape
     screen = screen[:, int(screen_height * 0.4):int(screen_height * 0.8)]
     view_width = int(screen_width * 0.6)
 
-    scale = screen_width / self.gym.x_threshold * 2
-    cart_location = int(self.gym.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
+    scale = screen_width / self.env.x_threshold * 2
+    cart_location = int(self.env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
 
     if cart_location < view_width // 2:
       slice_range = slice(view_width)
